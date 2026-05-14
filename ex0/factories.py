a8ff3255d54437d274388dfb5d11a4862fce25fd
Chia20-1 <chiachun2024@gmail.com>
2026-05-14 16:19:21 +0800

    Done ex0

---
+++ ex0/factories.py
@@ -0,0 +1,19 @@
+from .creature import Creature
+from .creature_factory import CreatureFactory
+from .creatures import Aquabub, Flameling, Pyrodon, Torragon
+
+
+class FlameFactory(CreatureFactory):
+    def create_base(self) -> Creature:
+        return Flameling()
+
+    def create_evolved(self) -> Creature:
+        return Pyrodon()
+
+
+class AquaFactory(CreatureFactory):
+    def create_base(self) -> Creature:
+        return Aquabub()
+
+    def create_evolved(self) -> Creature:
+        return Torragon()
