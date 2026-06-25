10ab64071c86687af3abe9e914ea5e2d556088d3
Chia20-1 <chiachun2024@gmail.com>
2026-06-25 21:02:22 +0800

    Done ex2

--- tournament.py
+++ tournament.py
@@ -5,16 +5,39 @@ from ex1 import HealingCreatureFactory, TransformCreatureFactory
 from ex2 import (
     BattleError,
     BattleStrategy,
-    NormalStrategy, 
-    AggressiveStrategy, 
+    NormalStrategy,
+    AggressiveStrategy,
     DefensiveStrategy,
 )
 
-opponent: TypeAlias = tuple[CreatureFactory, BattleStrategy]
-combatant: TypeAlias = tuple[Creature, BattleStrategy]
 
-def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
-    pass
+Opponent: TypeAlias = tuple[CreatureFactory, BattleStrategy]
+Combatant: TypeAlias = tuple[Creature, BattleStrategy]
+
+
+def battle(opponents: list[Opponent]) -> None:
+    print("*** Tournament ***")
+    print(f"{len(opponents)} opponents involved.\n")
+
+    combatants: list[Combatant] = [
+        (factory.create_base(), strategy)
+        for factory, strategy in opponents
+    ]
+    try:
+        for first_index in range(len(combatants)):
+            for second_index in range(first_index + 1, len(combatants)):
+                creature_one, strategy_one = combatants[first_index]
+                creature_two, strategy_two = combatants[second_index]
+                print(creature_one.describe())
+                print(" vs.")
+                print(creature_two.describe())
+                print(" now fight!")
+                strategy_one.act(creature_one)
+                strategy_two.act(creature_two)
+                print()
+    except BattleError as e:
+        print("BattleError, aborting tournament: ", e)
+
 
 if __name__ == "__main__":
     aqua_factory = AquaFactory()
@@ -25,3 +48,24 @@ if __name__ == "__main__":
     normal = NormalStrategy()
     aggressive = AggressiveStrategy()
     defensive = DefensiveStrategy()
+
+    basic_opponents = [(flame_factory, normal), (healing_factory, defensive)]
+    error_opponents = [
+        (flame_factory, aggressive),
+        (healing_factory, defensive),
+    ]
+    multiple_opponents = [
+        (aqua_factory, normal),
+        (healing_factory, defensive),
+        (transform_factory, aggressive),
+    ]
+
+    print("Tournament 0 (basic)")
+    print("[ (Flameling+Normal), (Healing+Defensive) ]")
+    battle(basic_opponents)
+    print("Tournament 1 (error)")
+    print("[ (Flameling+Aggressive), (Healing+Defensive) ]")
+    battle(error_opponents)
+    print("Tournament 2 (multiple)")
+    print("[ (Aquabub+Normal), (Healing+Defensive), (Transform+Aggressive) ]")
+    battle(multiple_opponents)
